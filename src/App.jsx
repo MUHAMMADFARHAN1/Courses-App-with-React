bf3934e62532bf0ba8901f6563252e4cc49f43ec
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-03 11:12:31 +0200

    react router included

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home_Page from "./Components/Home_Page";
+import { Link, NavLink, Route, Routes } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Home_Page />
