c454d739e5ba5251c342812c11358f10b14310d3
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-03 12:55:02 +0200

    basic app generated!

--- src/Components/Course_tab.jsx
+++ src/Components/Course_tab.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink, Route, Routes } from "react-router";
 
 function Course_tab(props) {
   return (
@@ -6,7 +7,13 @@ function Course_tab(props) {
       <p>{props.className}</p>
       {/* <p>Registered</p> */}
       <div className="flex flex-row justify-between bg-gray-100  rounded w-1/7">
-        <button className="bg-gray-300 rounded px-2 py-1">Details</button>
+        <NavLink
+          className="bg-gray-300 rounded px-2 py-1"
+          to={`course/${props.classid}`}
+        >
+          Details
+        </NavLink>
+        {/* <button className="bg-gray-300 rounded px-2 py-1">Details</button> */}
         <button className="bg-gray-300 rounded px-2 py-1">Registered</button>
       </div>
     </div>
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -7,13 +7,13 @@ import { Link, NavLink, Route, Routes } from "react-router";
 function List() {
   return (
     <div className="flex justify-between flex-col w-4/5 mx-auto mt-14 bg-white px-14 py-4 rounded gap-10">
-      <Course_tab className={data[0].name} />
-      <Course_tab className={data[1].name} />
-      <Course_tab className={data[2].name} />
-      <Course_tab className={data[3].name} />
-      <Course_tab className={data[4].name} />
-      <Course_tab className={data[5].name} />
-      <Course_tab className={data[6].name} />
+      <Course_tab className={data[0].name} classid={data[0].id} />
+      <Course_tab className={data[1].name} classid={data[1].id} />
+      <Course_tab className={data[2].name} classid={data[2].id} />
+      <Course_tab className={data[3].name} classid={data[3].id} />
+      <Course_tab className={data[4].name} classid={data[4].id} />
+      <Course_tab className={data[5].name} classid={data[5].id} />
+      <Course_tab className={data[6].name} classid={data[6].id} />
     </div>
   );
 }
