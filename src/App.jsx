5b562bdae43e458876ad603a802dd9e467a41ae6
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-03 11:39:36 +0200

    Two routes Created!

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home_Page from "./Components/Home_Page";
+import About from "./Components/About";
 import { Link, NavLink, Route, Routes } from "react-router";
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home_Page />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
