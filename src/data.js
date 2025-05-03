31e4183526ea84481ceb6bd60cd118fe5f15a240
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-03 13:22:04 +0200

    numbers corrected for navigation!

--- src/data.js
+++ src/data.js
@@ -4,78 +4,78 @@ const data = [
     description:
       "Quickly inspect page layouts and visualize element boundaries.",
     isActive: true,
-    id: 1,
+    id: 0,
   },
   {
     name: "Chemistry",
     description: "Instantly analyze and copy CSS from any webpage element.",
     isActive: true,
-    id: 2,
+    id: 1,
   },
   {
     name: "Mathematics",
     description: "Optimizes browser resource usage to accelerate page loading.",
     isActive: false,
-    id: 3,
+    id: 2,
   },
   {
     name: "Psycology",
     description:
       "Formats, validates, and prettifies JSON responses in-browser.",
     isActive: true,
-    id: 5,
+    id: 3,
   },
   {
     name: "Sociology",
     description: "Organizes browser tabs into groups and sessions.",
     isActive: true,
-    id: 6,
+    id: 4,
   },
   {
     name: "Arts",
     description:
       "Simulates various screen resolutions directly within the browser.",
     isActive: false,
-    id: 7,
+    id: 5,
   },
   {
     name: "Computer Science",
     description:
       "Enables annotation and notes directly onto webpages for collaborative debugging.",
     isActive: true,
-    id: 8,
+    id: 6,
   },
   {
     name: "Mechanics",
     description:
       "Overlay customizable grids and alignment guides on any webpage.",
     isActive: false,
-    id: 9,
+    id: 7,
   },
   {
     name: "Statistics",
     description: "Instantly extracts color palettes from any webpage.",
     isActive: true,
-    id: 10,
+    id: 8,
   },
   {
     name: "Painting",
     description: "Scans and highlights broken links on any page.",
     isActive: true,
-    id: 11,
+    id: 9,
   },
   {
     name: "DOM Snapshot",
     description: "Capture and export DOM structures quickly.",
     isActive: false,
-    id: 12,
+    id: 10,
   },
   {
     name: "ConsolePlus",
     description:
       "Enhanced developer console with advanced filtering and logging.",
     isActive: true,
-    id: 13,
+    id: 11,
   },
 ];
 
