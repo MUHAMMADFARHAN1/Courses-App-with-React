3124482d3c1dc8e77d1418697fddff029e5ab05d
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-03 10:28:42 +0200

    Navbar Generated!

--- src/Components/Home_Page.jsx
+++ src/Components/Home_Page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 function Home_Page() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default Home_Page;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import List from "./List";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <div className=" flex justify-between flex-row w-4/5 mx-auto mt-14 bg-white px-14 py-4 rounded">
+      <button className="bg-gray-300 rounded p-2">Home </button>
+      <button className="bg-gray-300 rounded p-2">About us</button>
+    </div>
+  );
 }
 
 export default Navbar;
