4c2c578bdf64c9922f595f562dc99effe71f96ec
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-10 12:33:07 +0200

    button in shadcn changed!

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { useState } from "react";
 import List from "./List";
 import { Link, NavLink, Route, Routes } from "react-router";
-
+// import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 //Modes are all decided on the top level so they donot necessarily need a context or state maintanence
 
 function Navbar() {
@@ -16,11 +17,13 @@ function Navbar() {
   return (
     <div>
       <div className=" flex justify-between flex-row w-4/5 mx-auto mt-14 bg-white dark:bg-gray-100 px-14 py-4 rounded -mb-10 gap-2">
-        <NavLink className="bg-gray-300 dark:bg-white rounded p-2" to="/">
+        <Button size="icon" className="bg-white text-black">
           Home
-        </NavLink>
+        </Button>
         <div className="flex justify-between flex-row xl:w-1/5 gap-2">
-          <button className="bg-gray-300 dark:bg-white rounded p-2">All</button>
+          <Button size="icon" className="bg-white text-black">
+            All
+          </Button>
           <button className="bg-gray-300 dark:bg-white rounded p-2">
             Registered
           </button>
@@ -31,19 +34,21 @@ function Navbar() {
             About us
           </NavLink>
           {isDark ? (
-            <button
-              className="bg-gray-300 rounded p-2"
+            <Button
+              size="icon"
+              className="bg-white text-black"
               onClick={() => toggleMode(false)}
             >
               Dark
-            </button>
+            </Button>
           ) : (
-            <button
-              className="bg-white  rounded p-2"
+            <Button
+              size="icon"
+              className="bg-white text-black"
               onClick={() => toggleMode(true)}
             >
               Light
-            </button>
+            </Button>
           )}
         </div>
       </div>
